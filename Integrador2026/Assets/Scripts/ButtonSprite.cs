b5d6df4dae1814c69b191e144d16a3a709df4d55
Luis Felipe Tavares Mendes <00124368@unisoacad.uniso.br>
2026-03-10 22:01:21 -0300

    Começo

---
+++ Integrador2026/Assets/Scripts/ButtonSprite.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonSprite : MonoBehaviour
+{
+    public Button button1;
+    public Button button2;
+    public Button button3;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
