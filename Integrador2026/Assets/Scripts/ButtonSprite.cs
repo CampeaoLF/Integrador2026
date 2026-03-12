6f88eaeaa020050acf34c1d13dd0ac15cd60135c
Luis Felipe Tavares Mendes <00124368@unisoacad.uniso.br>
2026-03-11 21:44:55 -0300

    evoluçao

--- Integrador2026/Assets/Scripts/ButtonSprite.cs
+++ Integrador2026/Assets/Scripts/ButtonSprite.cs
@@ -5,18 +5,35 @@ using UnityEngine.UI;
 
 public class ButtonSprite : MonoBehaviour
 {
-    public Button button1;
-    public Button button2;
-    public Button button3;
+    public Button[] Botoes;
+    public Sprite[] sprites;
 
     void Start()
     {
         
     }
 
-    
     void Update()
     {
-        
+        if (Input.touchCount == 1)
+        {
+            if (Botoes != null && Botoes.Length > 1 && sprites != null && sprites.Length > 0)
+            {
+                Image img = Botoes[0].GetComponent<Image>();
+                if (img != null)
+                {
+                    img.sprite = sprites[0];
+                }
+            }
+
+            if (Botoes != null && Botoes.Length > 2 && sprites != null && sprites.Length > 1)
+            {
+                Image img = Botoes[1].GetComponent<Image>();
+                if (img != null)
+                {
+                    img.sprite = sprites[0];
+                }
+            }
+        }
     }
 }
