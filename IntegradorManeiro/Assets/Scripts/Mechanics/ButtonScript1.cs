2bdb35473592d70fff9bf3756d76f1ae6243db31
Luis Felipe Tavares Mendes <00124368@unisoacad.uniso.br>
2026-03-24 21:42:56 -0300

    fusion

---
+++ IntegradorManeiro/Assets/Scripts/Mechanics/ButtonScript1.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class ButtonScript : MonoBehaviour
+{
+    [SerializeField] public GameObject player;
+    [SerializeField] public GameManager gameManager;
+
+    [Header("Botão")]
+    [SerializeField] public Button[] buttons;
+
+    [Header("Sprite")]
+    [SerializeField] public Sprite[] sprites;
+    [SerializeField] public Sprite[] cenario;
+
+
+    void Start()
+    {
+        
+       
+
+    }
+
+
+    void Update()
+    {
+
+        if (gameManager.score >= 5 && cenario[0])
+        {
+
+            var scenarioAtual = gameManager.background.GetComponent<SpriteRenderer>();
+            scenarioAtual.sprite = cenario[0];
+        }
+        if (gameManager.score >= 10 && cenario[1])
+        {
+            var scenarioAtual = gameManager.background.GetComponent<SpriteRenderer>();
+            scenarioAtual.sprite = cenario[1];
+        }
+
+
+
+    }
+
+    public void Apertar(Button botao)
+    {
+        
+
+        if (Input.touchCount == 1 && buttons[0])
+        {
+            
+            var spriteAtual = player.GetComponent<SpriteRenderer>();
+            spriteAtual.sprite = sprites[0];
+            gameManager.GainPoints(1);
+            
+        }
+        
+    }
+
+    public void Apertar2()
+    {
+        if (Input.touchCount == 1 && buttons[1])
+        {
+            
+            var spriteAtual = player.GetComponent<SpriteRenderer>();
+            spriteAtual.sprite = sprites[1];
+            gameManager.GainPoints(5);
+
+        }
+
+    }
+
+    public void Apertar3()
+    {
+        if (Input.touchCount == 1 && buttons[2])
+        {
+            
+            var spriteAtual = player.GetComponent<SpriteRenderer>();
+            spriteAtual.sprite = sprites[2];
+            gameManager.GainPoints(5);
+
+        }
+
+    }
+
+    public void Apertar4(Button botao)
+    {
+        if (Input.touchCount == 1 && buttons[3])
+        {
+            var spriteAtual = player.GetComponent<SpriteRenderer>();
+            spriteAtual.sprite = sprites[3];
+            gameManager.GainPoints(10);
+        }
+       
+    }
+
+}
